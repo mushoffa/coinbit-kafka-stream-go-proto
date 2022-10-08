fd148d7a34c6f2161fcc79f4958503df1e665104
mushoffa <ridwan.mushoffa@gmail.com>
2022-10-09 03:51:17 +0700

    Add wallet codec

---
+++ codec/wallet_codec.go
@@ -0,0 +1,24 @@
+package codec
+
+import (
+	"fmt"
+
+	"github.com/mushoffa/coinbit-kafka-stream-go-proto/pb"
+
+	"google.golang.org/protobuf/proto"
+)
+
+type WalletCodec struct {}
+
+func (c *WalletCodec) Encode(value interface{}) ([]byte, error) {
+	if _, wallet := value.(*pb.Wallet); !wallet {
+		return nil, fmt.Errorf("Expected *pb.Wallet type, got %T", value)
+	}
+
+	return proto.Marshal(value.(*pb.DepositMoney))
+}
+
+func (c *WalletCodec) Decode(data []byte) (interface{}, error) {
+	var wallet pb.Wallet
+	return &wallet, proto.Unmarshal(data, &wallet)
+}
